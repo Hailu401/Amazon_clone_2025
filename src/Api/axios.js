ed80839900aca11f866b47813362c6245888f340
Hailu401 <123373063+Hailu401@users.noreply.github.com>
2025-06-01 11:50:39 +0300

    responsiveness amended

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,7 +3,7 @@ const AxiosInstance = Axios.create({
   //deploye server on render
     baseURL: "https://amazon-api-uhi9.onrender.com",
 //for local server
-  baseURL: "http://localhost:5005",
+  // baseURL: "http://localhost:5005",
 });
 
 export { AxiosInstance };
